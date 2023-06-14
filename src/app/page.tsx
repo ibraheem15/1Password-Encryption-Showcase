09adc19e166c3842ddbb9d58b2062e71b6f058e6
ibraheem15 <ibraheemrehman15@gmail.com>
2023-06-14 11:54:02 +0500

    Imporved Interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -114,12 +114,12 @@ export default function Home() {
   };
   return (
     <div
-      className={` max-w-5xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 top-0 bottom-0 left-0 right-0 m-auto absolute mt-10 mb-10 text-center`}
+      className={` p-6 max-w-4xl max-sm:mt-0 max-sm:mb-0 max-sm:relative bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 top-0 bottom-0 left-0 right-0 m-auto mt-10 mb-10 text-center  `}
     >
       <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl dark:text-white">
         Encryption and Decryption
       </h1>
-      <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+      <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 dark:text-gray-400">
         This encryption uses AES-256-GCM and ECDH which is a key agreement
         protocol that allows two parties, each having an elliptic-curve
         public-private key pair, to establish a shared secret over an insecure
@@ -133,41 +133,18 @@ export default function Home() {
           Client 1
         </h5>
         <form className={``} method="post" onSubmit={handleSubmit}>
-          <label
-            htmlFor="search"
-            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-          >
-            Search
-          </label>
-          <div className="relative">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                ></path>
-              </svg>
-            </div>
+          <div className="flex">
             <input
               type="search"
               id="search"
               name="messageinput"
-              className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="block w-full p-4 m-1 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Encrypted Message"
               required
             />
             <button
               type="submit"
-              className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white m-1 right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Encrypt
             </button>
